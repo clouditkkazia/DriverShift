f1e7412839650d9d5efb334a49066f1143113508
kkazia <33987413+kkazia@users.noreply.github.com>
2025-03-03 06:28:06 -0500

    changes to views and helpers

--- helpers.php
+++ helpers.php
@@ -23,3 +23,44 @@ function loadView($name)
 {
     require basePath("views/{$name}.view.php");
 }
+
+
+function loadPartials($name)
+{
+    $viewPartial = basePath("views/partials/{$name}.php");
+    if (file_exists($viewPartial)) {
+        require $viewPartial;
+    } else {
+        echo "Partial '{$name} not found!!'";
+    }
+}
+
+/**
+ * Inspect a Value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+}
+
+
+/**
+ * Inspect a Value(s) and Die another day!!!
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+    die();
+}
--- views/home.view.php
+++ views/home.view.php
@@ -1,4 +1,4 @@
 <?php
-require basePath('views/partials/header.php');
-require basePath('views/partials/navbar.php');
-require basePath('views/partials/footer.php');
+loadPartials('header');
+loadPartials('navbar');
+loadPartials('footer');
